c1398338dc51e5aa5b1bd2021abc0038ea0b1f99
omargamal510 <34138436+omargamal510@users.noreply.github.com>
2024-09-20 00:35:09 +0300

    [ADD] blog n slider

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -4,6 +4,8 @@ tailwind.config = {
       colors: {
         clifford: "#da373d",
         mainbgblue: "#011e26",
+        mainbgblue2: "#213c4786",
+        bgWrapper: "#00000073",
         customGray1: "#3d4144",
         whiteShadow: "#FFF",
         redShadowSpread: "0 4px 20px 20px rgba(255, 0, 0, 0.5)", // X, Y, blur, spread, color
