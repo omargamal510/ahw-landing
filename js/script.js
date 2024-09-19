20e1705f22163bf45b8c55c820518a3270781129
omargamal510 <34138436+omargamal510@users.noreply.github.com>
2024-09-19 12:04:00 +0300

    [MODIFY,ADD] Big navbar , add sm navbar for small screens

--- js/script.js
+++ js/script.js
@@ -15,21 +15,47 @@ const userBtn = document
 
 // ==
 
-function greetUser() {
+function greetUser(userName) {
   const time = new Date();
   const hours = time.getHours();
   const greetingElement = document.querySelector(".user-greeting-text");
 
-  if (hours >= 6 && hours < 12) {
-    greetingElement.textContent = "صباح الخير , عمر";
-  } else if (hours >= 12 && hours < 18) {
-    greetingElement.textContent = "مساء الخير , عمر";
+  if (hours < 12) {
+    greetingElement.textContent = `صباح الخير , ${userName}`;
   } else {
-    greetingElement.textContent = "مساء الخير , عمر";
+    greetingElement.textContent = `مساء الخير , ${userName}`;
   }
 }
 
-window.onload = greetUser;
+document.addEventListener("DOMContentLoaded", function () {
+  greetUser("عمر");
+});
+
+const smNavComponent = document.querySelector(".sm-nav-holder");
+const smNav = document.querySelector(".sm-nav");
+
+function handleSmNavToggle() {
+  smNavComponent.classList.remove("bg-transparent");
+  smNav.classList.remove("sm-nav-closed");
+  smNavComponent.classList.remove("sm-nav-closed");
+}
+
+const smNavToggle = document
+  .querySelector(".sm-nav-toggle")
+  .addEventListener("click", handleSmNavToggle);
+
+function handleSmNavClose() {
+  smNavComponent.classList.add("bg-transparent");
+  smNav.classList.add("sm-nav-closed");
+
+  setTimeout(() => {
+    smNavComponent.classList.add("sm-nav-closed");
+  }, 250);
+}
+
+const smNavClose = document
+  .querySelector(".sm-nav-close")
+  .addEventListener("click", handleSmNavClose);
 
 // ==
 
